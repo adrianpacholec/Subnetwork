ee9bd2215bf61f376a40be490f91627a86abd11a
maciekkobierecki <maciekkobierecki@gmail.com>
2017-12-16 11:35:18 +0100

    0.0.2

--- Subnetwork/SubnetworkServer.cs
+++ Subnetwork/SubnetworkServer.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CustomSocket;
-
 
 namespace Subnetwork
 {
-    class ConnectionController
+    class SubnetworkServer
     {
-        private List<CSocket> sockets;
-
-        
     }
 }
