4d163a349ff2b95b862b49c96d8d25e6c1047735
maciekkobierecki <maciekkobierecki@gmail.com>
2017-12-16 10:10:40 +0100

    0.0.1
    added config class

---
+++ Subnetwork/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelSwitchingRouter
+{
+    class Config
+    {
+        public static String getProperty(String key)
+        {
+            String value = ConfigurationManager.AppSettings[key];
+            return value;
+        }
+        public static int getIntegerProperty(String key)
+        {
+            String value = ConfigurationManager.AppSettings[key];
+            int intValue = int.Parse(value);
+            return intValue;
+        }
+    }
+}
